35f1bd5c75dc20846a671f33a755bf5af8b7da13
HogHordGit <hoghord@gmail.com>
2022-11-29 18:25:20 +0200

    test

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -1,13 +1,11 @@
-const header = document.querySelector(".page-header__navbar");
-const block = document.querySelector(".navbar-collapse__block");
 
-function handleCart(e) {
-    const target = e.target;
-    
-    if (target && (target.classList.contains("fa-shopping-cart") || 
-    target.classList.contains("page-header__cart-btn"))) {
-        block.classList.toggle("-active");
-    }
-}
 
-header.addEventListener("click", handleCart);
+
+
+
+
+
+
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,9 @@
 const contentContainer = document.querySelector("#content-container");
 const cartCounterLabel = document.querySelector("#cart-counter-label");
 
+const headerContainer = document.querySelector(".navbar-collapse");
+
+let Goods = [];
 let cartCounter = 0;
 let cartPrise = 0;
 
@@ -24,6 +27,19 @@ const enableControls = (t, func) => {
     t.disabled = false;
     contentContainer.addEventListener("click", func);
 };
+
+const getPhotoGood = (t) => {
+    let link = t.parentElement.parentElement.parentElement.firstElementChild.firstElementChild.src;
+
+    console.log(link);
+    // return replace(//);
+};
+
+const getNameGood = (t) => t.parentElement.
+                        parentElement.
+                        firstElementChild.
+                        innerHTML;
+
 
 const getMockData = (t) => +t.
             parentElement.
@@ -57,4 +73,33 @@ const btnClickHandler = function(e) {
     }
 };
 
+//Cart_btn------------------------------------------------------
+
+function handleCart(e) {
+    const block = document.querySelector(".cart-block");
+    const textZero = document.querySelector(".cart-zero");
+
+    const target = e.target;
+    
+    if (target && (target.classList.contains("fa-shopping-cart") || 
+    target.classList.contains("page-header__cart-btn"))) {
+        block.classList.toggle("-active");
+    }
+    
+    if (cartCounter === 0) {
+        textZero.style.display = "block";
+    } else {
+        textZero.style.display = "none";
+        addGoodIntoArr(target);
+    }
+}
+
+function addGoodIntoArr(t) {
+    let prise = getMockData(t);
+    let name = getNameGood(t);
+    getPhotoGood(t);
+
+}
+
 contentContainer.addEventListener("click", btnClickHandler);
+document.body.addEventListener("click", handleCart);
